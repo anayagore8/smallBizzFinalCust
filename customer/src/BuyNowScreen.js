b641c424c6bbd2115bddc9bab46ab2856434bd41
anayagore8 <anayagore0502@gmail.com>
2024-06-18 17:47:08 +0530

    Anaya cust

--- customer/src/BuyNowScreen.js
+++ customer/src/BuyNowScreen.js
@@ -1,58 +1,87 @@
-// BuyNowScreen.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import QuantityCounter from './QuantityCounter';
 
 const BuyNowScreen = ({ userId }) => {
-  const { productId } = useParams(); // Get productId from URL params
+  const { productId } = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
-    // Fetch product details based on productId
     const fetchProductDetails = async () => {
-        try {
-          const response = await fetch(`http://localhost:5000/products/${productId}`);
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const data = await response.json();
-          setProduct(data); // Set product details
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching product details:', error);
-          // Handle error state
-          setLoading(false);
+      try {
+        const response = await fetch(`http://localhost:5000/products/${productId}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
+        const data = await response.json();
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching product details:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchProductDetails();
+  }, [productId]);
+
+//   const handleBuyNow = async () => {
+//     try {
+//       const data = {
+//         productId: product._id,
+//         quantity: quantity, // Add quantity to data object
+//         shopId: product.shopId // Add shopId to data object
+//       };
+//       await axios.post('http://localhost:5000/buy-now', data);
+//       console.log('Product bought successfully');
+//       //navigate('/'); // Redirect to home page after buying
+//     } catch (error) {
+//       console.error('Error buying product:', error);
+//     }
+// };
+const handleBuyNow = async () => {
+  try {
+    const data = {
+      orderDate: new Date(), // Add orderDate
+      productName: product.name, // Add productName
+      productId: product._id, // Add productId
+      quantity: quantity, // Add quantity
+      shopId: product.shopId, // Add shopId
+      customerId: userId, // Add customerId
+      orderStatus: 'pending' // Add orderStatus
+    };
+    await axios.post('http://localhost:5000/buy-now/', data);
+    console.log('Product bought successfully');
+    //navigate('/'); // Redirect to home page after buying
+  } catch (error) {
+    console.error('Error buying product:', error);
+  }
+};
+
+
+
+const handleAddToCart = async () => {
+  try {
+      const data = {
+          userId: userId,
+          productId: product._id,
+          productName: product.name,
+          price: product.price,
+          description: product.description,
+          quantity: quantity,
+          shopId: product.shopId // Include the shopId here
       };
-  
-      fetchProductDetails();
-    }, [productId]);
-      
-
-  const handleBuyNow = () => {
-    // Implement buy now functionality
-    console.log('Buy now clicked');
-  };
-
-  const handleAddToCart = async () => {
-    try {
-        const data = {
-            userId:userId, // Use props.userId to access userId
-            productId: product._id,
-            productName: product.name,
-            price: product.price,
-            description: product.description
-        };
-        await axios.post('http://localhost:5000/add-to-cart', data);
-        console.log('Product added to cart');
-        navigate('/cart');
-
-    } catch (error) {
-        console.error('Error adding product to cart:', error);
-    }
+      await axios.post('http://localhost:5000/add-to-cart', data);
+      console.log('Product added to cart');
+      navigate('/cart');
+  } catch (error) {
+      console.error('Error adding product to cart:', error);
+  }
 };
 
 
@@ -66,6 +95,10 @@ const BuyNowScreen = ({ userId }) => {
           <p>Description: {product.description}</p>
           <img src={product.image} alt={product.name} />
           <p>Price: {product.price}</p>
+          <QuantityCounter
+            quantity={quantity}
+            onChange={(newQuantity) => setQuantity(newQuantity)}
+          />
           <button onClick={handleBuyNow}>Buy Now</button>
           <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
--- customer/src/components/CartList.js
+++ customer/src/components/CartList.js
@@ -1,15 +1,15 @@
-// CartList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Counter from './Counter'; // Import the Counter component
-import './cartItem.css'; // Import the CSS file
+import Counter from './Counter';
+import './cartItem.css';
+import QuantityCounter from '../QuantityCounter';
 
 function CartList({ userId }) {
   const [cartItems, setCartItems] = useState([]);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     if (userId) {
-      // Fetch cart items for the logged-in user
       axios.get(`http://localhost:5000/cart/${userId}/items`)
         .then(response => {
           setCartItems(response.data);
@@ -21,18 +21,14 @@ function CartList({ userId }) {
   }, [userId]);
 
   const handleCountUpdate = (itemId, newCount) => {
-    // Update the count of the cart item in the database
     axios.put(`http://localhost:5000/cart/${itemId}/count`, { count: newCount })
       .then(response => {
-        // Update the local state if the database update is successful
         if (newCount === 0) {
-          // If the count becomes zero, delete the item
           handleDeleteItem(itemId);
         } else {
           setCartItems(prevItems => {
             const updatedItems = prevItems.map(item => {
               if (item._id === itemId) {
-                // Update the count of the specific item
                 return { ...item, count: newCount };
               }
               return item;
@@ -44,19 +40,40 @@ function CartList({ userId }) {
       .catch(error => {
         console.error('Error updating count:', error);
       });
-};
+  };
 
-const handleDeleteItem = (itemId) => {
+  const handleDeleteItem = (itemId) => {
     axios.delete(`http://localhost:5000/cart/${itemId}`)
       .then(response => {
-        // Remove the item from the cart items state
         setCartItems(prevItems => prevItems.filter(item => item._id !== itemId));
       })
       .catch(error => {
         console.error('Error deleting item:', error);
       });
-};
+  };
 
+  const handleBuyNow = async (productId) => {
+    try {
+      const product = cartItems.find(item => item.productId === productId);
+      const data = {
+        orderDate: new Date(),
+        productName: product.productName,
+        productId: product.productId,
+        quantity: product.count,
+        shopId: product.shopId,
+        customerId: userId,
+        orderStatus: 'pending'
+      };
+      await axios.post('http://localhost:5000/buy-now', data);
+      console.log('Product bought successfully');
+      
+      // Clear the particular product from the cart items state
+      setCartItems(prevItems => prevItems.filter(item => item._id !== product._id));
+    } catch (error) {
+      console.error('Error buying product:', error);
+    }
+  };
+  
 
   return (
     <div>
@@ -67,9 +84,14 @@ const handleDeleteItem = (itemId) => {
             <h3>{item.productName}</h3>
             <p>Price: {item.price}</p>
             <p>Description: {item.description}</p>
-            <Counter initialValue={item.count} onUpdate={newCount => handleCountUpdate(item._id, newCount)} />
+            {/* <Counter initialValue={item.count} onUpdate={newCount => handleCountUpdate(item._id, newCount)} /> */}
+            <QuantityCounter
+            quantity={quantity}
+            onChange={(newQuantity) => setQuantity(newQuantity)}
+          />
             <p>Count in Cart: {item.count}</p>
             <button onClick={() => handleDeleteItem(item._id)}>Delete</button>
+            <button onClick={() => handleBuyNow(item.productId)}>Buy Now</button>
           </div>
         ))}
       </div>
